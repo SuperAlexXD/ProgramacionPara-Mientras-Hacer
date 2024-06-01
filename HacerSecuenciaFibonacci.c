9a7a692fe81039c842e6d7a21c785c5663d8f377
SuperAlexXD <villamaguasantiago@gmail.com>
2024-06-01 10:53:24 -0500

    HacerSecuenciaFibonacci.c

---
+++ HacerSecuenciaFibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int secuencia;
+int proyectar = 1; 
+int sumatoria = 0; 
+
+int main(int argc, char const *argv[])
+{
+    printf("Ingrese la cantidad de números que quieres que aparezcan en la secuencia: ");
+    scanf("%i", &secuencia);
+
+    if (secuencia == 1) {
+        printf("0\n");
+        return 0;
+    }
+    else if (secuencia == 2) {
+        printf("0, 1\n");
+        return 0;
+    }
+
+    printf("0, 1");  
+
+int i = 2;
+
+      do
+    {
+        int siguiente = proyectar + sumatoria;
+        printf(", %d", siguiente);
+
+        sumatoria = proyectar;
+        proyectar = siguiente;
+        i++;
+    }
+    while (i < secuencia);
+  
+    printf("\n");
+    return 0;
+}
