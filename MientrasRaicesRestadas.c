450671f718e400a92f2a2cca63d1ab3426f5acc7
SuperAlexXD <villamaguasantiago@gmail.com>
2024-06-02 09:07:06 -0500

    MientrasRaicesRestadas.c

---
+++ MientrasRaicesRestadas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int radical = 3;
+    int numero;
+    
+    printf("Ingrese el numero que desea reducir: ");
+    scanf("%i", &numero);
+    
+    int resultado = 0;
+    int cubo = 0;
+    
+    while (cubo <= numero) {
+        resultado++;
+        cubo = resultado * resultado * resultado;
+    }
+
+    resultado--;
+    
+    printf("Raíz cúbica: %d\n", resultado);
+    
+    return 0;
+}
