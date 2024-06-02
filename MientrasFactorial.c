531290ccae6cf9693f12b1d83578722f71afdfd8
SuperAlexXD <villamaguasantiago@gmail.com>
2024-06-02 09:25:30 -0500

    MientrasFactorial.c

---
+++ MientrasFactorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int fact;
+    int multiplicar = 1;
+
+    printf("Ingrese un numero para calcular su factorial: \n");
+    scanf("%i", &fact);
+
+    int i = 1; 
+    while(i <= fact)
+    {
+        multiplicar = multiplicar * i;
+        i++;
+    }
+    
+    printf("El factorial de %d es: %d\n", fact, multiplicar);
+    return 0;
+}
