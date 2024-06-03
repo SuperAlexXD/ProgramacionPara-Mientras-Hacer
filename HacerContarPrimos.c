fde266fe60d9bf903576a0bc093b079e1bcac74a
SuperAlexXD <villamaguasantiago@gmail.com>
2024-06-03 03:54:45 -0500

    HacerContarPrimos.c

---
+++ HacerContarPrimos.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int esPrimo(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    int contador = 0;
+    int i = 2;
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &n);
+
+    if (n >= 2) {
+        do {
+            if (esPrimo(i)) {
+                contador++;
+            }
+            i++;
+        } while (i <= n);
+    }
+
+    printf("Hay %d numeros primos hasta %d\n", contador, n);
+
+    return 0;
+}
